6cb0773c9975b71d78063c7fb5bb9696f392f9a5
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2022-04-12 13:39:28 +0300

    common.js: settings.licenceinfo: URNs for some CC licences
    app/modes/common.js:
    - settings.licenceinfo: Replace direct URLs to Creative Commons licences
      with Language Bank URNs where available.

--- app/modes/common.js
+++ app/modes/common.js
@@ -104,12 +104,14 @@ settings.licenceinfo = {
     CC0: {
         name: "CC ZERO (CC0) (CLARIN PUB)",
         description: "Public Domain Dedication",
-        url: "http://creativecommons.org/publicdomain/zero/1.0/",
+        // URN to the English version (the same for all CC licences)
+        // TODO: Allow different URNs based on the UI language
+        urn: "urn:nbn:fi:lb-2021120221",
     },
     CC_BY: {
         name: "CC BY (CLARIN PUB)",
         description: "Creative Commons Attribution",
-        url: "https://creativecommons.org/licenses/by/4.0/",
+        urn: "urn:nbn:fi:lb-2021120223",
     },
     CC_BY_30: {
         name: "CC BY 3.0 (CLARIN PUB)",
@@ -119,22 +121,22 @@ settings.licenceinfo = {
     CC_BY_40: {
         name: "CC BY 4.0 (CLARIN PUB)",
         description: "Creative Commons Attribution",
-        url: "https://creativecommons.org/licenses/by/4.0/",
+        urn: "urn:nbn:fi:lb-2021120223",
     },
     CC_BY_NC: {
         name: "CC BY-NC (CLARIN PUB)",
         description: "Creative Commons Attribution-NonCommercial",
-        url: "https://creativecommons.org/licenses/by-nc/4.0/",
+        urn: "urn:nbn:fi:lb-2021120227",
     },
     CC_BY_ND: {
         name: "CC BY-ND (CLARIN PUB)",
         description: "Creative Commons Attribution-NoDerivatives",
-        url: "https://creativecommons.org/licenses/by-nd/4.0/",
+        urn: "urn:nbn:fi:lb-2021120231",
     },
     CC_BY_ND_40: {
         name: "CC BY-ND 4.0 (CLARIN PUB)",
         description: "Creative Commons Attribution-NoDerivatives 4.0",
-        url: "https://creativecommons.org/licenses/by-nd/4.0/",
+        urn: "urn:nbn:fi:lb-2021120231",
     },
     CC_BY_NC_ND: {
         name: "CC BY-NC-ND (CLARIN PUB)",
