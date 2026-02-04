15f8e9cfa73ea55a4b42218c5cf25d9829f9d9f6
eaxelson <erik.axelson@helsinki.fi>
2026-02-04 16:01:08 +0200

    Update metadata link of erme_psla.

--- app/modes/other_languages_mode.js
+++ app/modes/other_languages_mode.js
@@ -412,7 +412,7 @@ settings.corporafolders.uralic.erme_psla = {
     title: "ERME PSLA",
     description: "ERME: Erzya and Moksha Extended Corpora, Paragraph Segmentation, Low Annotation",
     info: {
-      metadata_urn: "urn:nbn:fi:lb-2023021601",
+      metadata_urn: "urn:nbn:fi:lb-2025121601",
         licence: {
             name: "CC BY (PUB)",
             urn: "urn:nbn:fi:lb-2022020106",
